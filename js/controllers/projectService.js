2b2bcf0c08592119ec25f68048da8b20308a868a
Kush <kushthaker@gmail.com>
2015-09-03 00:53:48 -0400

    added filler literal object in projectService

--- js/controllers/projectService.js
+++ js/controllers/projectService.js
@@ -4,3 +4,42 @@ function ProjectService() {
 
 
 angular.module('kush').service('projectService', ProjectService);
+
+
+var PROJECT_DATA = [{
+	"title": "mynews",
+	"links": {
+		"github": "https://github.com/kushthaker/mynews.git",
+		"demo": "http://mynews.kushthaker.com/",
+	},
+	"caption": "Using AngularJS to aggregate news from multiple external APIs.",
+	"description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+	"topic_1": "Technical",
+	"topic_1_content": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+	"topic_2": "Toughest Issue",
+	"topic_2_content": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"
+}, {
+	"title": "canyouth",
+	"links": {
+		"github": "https://github.com/kushthaker/mynews.git",
+		"demo": "http://mynews.kushthaker.com/",
+	},
+	"caption": "Using AngularJS to aggregate news from multiple external APIs.",
+	"description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+	"topic_1": "Technical",
+	"topic_1_content": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+	"topic_2": "Toughest Issue",
+	"topic_2_content": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"
+}, {
+	"title": "psdg",
+	"links": {
+		"github": "https://github.com/kushthaker/mynews.git",
+		"demo": "http://mynews.kushthaker.com/",
+	},
+	"caption": "Using AngularJS to aggregate news from multiple external APIs.",
+	"description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+	"topic_1": "Technical",
+	"topic_1_content": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+	"topic_2": "Toughest Issue",
+	"topic_2_content": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"
+}]
