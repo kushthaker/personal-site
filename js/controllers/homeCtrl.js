b99f11240eceb64f2f529e4cd26531a16541419a
Kush <kushthaker@gmail.com>
2015-09-22 09:18:34 -0400

    added work service objrk service object

--- js/controllers/homeCtrl.js
+++ js/controllers/homeCtrl.js
@@ -1,7 +1,10 @@
-function HomeCtrl(projectService) {
+function HomeCtrl(projectService, workService) {
 
 	this.projectService = projectService;
+	this.workService = workService;
+
 	this.projects = this.projectService.getProjects();	
+	this.workList = this.workService.getWorkList();	
 	
 }
 
--- js/controllers/workService.js
+++ js/controllers/workService.js
@@ -1,6 +1,99 @@
 function WorkService() {
 
+	this.work = this.getWorkList();
+
 }
 
 
 angular.module('kush').service('workService', WorkService);
+
+
+WorkService.prototype.getWorkList = function() {
+	return WORK_DATA;
+};
+
+var WORK_DATA = [{
+	"company": "mynews",
+	"logo": "img_here",
+	"role": "mynews",
+	"date": "Jan - Apr 2014",
+	"caption": "Using AngularJS to aggregate news from multiple external APIs.",
+	"links": {
+		"main": "https://github.com/kushthaker/mynews.git",
+		"github": "https://github.com/kushthaker/mynews.git",
+		"demo": "http://mynews.kushthaker.com/",
+	},
+	"description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"
+}, {
+	"company": "mynews",
+	"logo": "img_here",
+	"role": "mynews",
+	"date": "Jan - Apr 2014",
+	"caption": "Using AngularJS to aggregate news from multiple external APIs.",
+	"links": {
+		"main": "https://github.com/kushthaker/mynews.git",
+		"github": "https://github.com/kushthaker/mynews.git",
+		"demo": "http://mynews.kushthaker.com/",
+	},
+	"description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"
+}, {
+	"company": "mynews",
+	"logo": "img_here",
+	"role": "mynews",
+	"date": "Jan - Apr 2014",
+	"caption": "Using AngularJS to aggregate news from multiple external APIs.",
+	"links": {
+		"main": "https://github.com/kushthaker/mynews.git",
+		"github": "https://github.com/kushthaker/mynews.git",
+		"demo": "http://mynews.kushthaker.com/",
+	},
+	"description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"
+}, {
+	"company": "mynews",
+	"logo": "img_here",
+	"role": "mynews",
+	"date": "Jan - Apr 2014",
+	"caption": "Using AngularJS to aggregate news from multiple external APIs.",
+	"links": {
+		"main": "https://github.com/kushthaker/mynews.git",
+		"github": "https://github.com/kushthaker/mynews.git",
+		"demo": "http://mynews.kushthaker.com/",
+	},
+	"description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"
+}, {
+	"company": "mynews",
+	"logo": "img_here",
+	"role": "mynews",
+	"date": "Jan - Apr 2014",
+	"caption": "Using AngularJS to aggregate news from multiple external APIs.",
+	"links": {
+		"main": "https://github.com/kushthaker/mynews.git",
+		"github": "https://github.com/kushthaker/mynews.git",
+		"demo": "http://mynews.kushthaker.com/",
+	},
+	"description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"
+}, {
+	"company": "mynews",
+	"logo": "img_here",
+	"role": "mynews",
+	"date": "Jan - Apr 2014",
+	"caption": "Using AngularJS to aggregate news from multiple external APIs.",
+	"links": {
+		"main": "https://github.com/kushthaker/mynews.git",
+		"github": "https://github.com/kushthaker/mynews.git",
+		"demo": "http://mynews.kushthaker.com/",
+	},
+	"description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"
+}, {
+	"company": "mynews",
+	"logo": "img_here",
+	"role": "mynews",
+	"date": "Jan - Apr 2014",
+	"caption": "Using AngularJS to aggregate news from multiple external APIs.",
+	"links": {
+		"main": "https://github.com/kushthaker/mynews.git",
+		"github": "https://github.com/kushthaker/mynews.git",
+		"demo": "http://mynews.kushthaker.com/",
+	},
+	"description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"
+}]  
